0d6fbf2c507fd3658b3ff4fd0fab47dd52d3755d
Mozcko <musquiz02@gmail.com>
2023-08-24 03:49:54 -0600

    Update settings.py

--- crud_api/settings.py
+++ crud_api/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "client")
 
 STATICFILES_DIRS = [
   # Tell Django where to look for React's static files (css, js)
